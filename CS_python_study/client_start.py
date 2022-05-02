6ba5067f8532607cca3faf309e1b385a8d9f3cec
AntonZykov85 <zakupki_pomosh@mail.ru>
2022-05-02 11:04:46 +0300

    lesson_7_done with path_like for popen

--- CS_python_study/client_start.py
+++ CS_python_study/client_start.py
@@ -1,8 +1,16 @@
+import csv
 import subprocess
 import os
+import sys
 # from subprocess import Popen, CREATE_NEW_CONSOLE
 
+sys.path.append('../')
+PATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(os.getcwd(), '..'))
+
+# PATH = os.path.join(PATH, 'client.log')
 p_list = []
+args = ['/home/anton/PycharmProjects/Client_server/Client_server/CS_python_study','python client.py']
 while True:
     action = input('Start 10 clients (s) / '
                  'close client (x) /'
@@ -11,12 +19,11 @@ while True:
     if action == 'q':
         break
     elif action == 's':
-        p_list.append(subprocess.Popen('python server.py'))
+        p_list.append(subprocess.Popen((args), shell=True))
 
         for _ in range(10):
-            p_list.append(subprocess.Popen('python client.py'))
+            p_list.append(subprocess.Popen(args))
             print('10 clients are start')
-
     elif action == 'x':
         for p in p_list:
             p.kill()
