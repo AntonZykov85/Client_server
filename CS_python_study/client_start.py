5012351707898ac2906b56d9824e7aa803fda4e6
AntonZykov85 <zakupki_pomosh@mail.ru>
2022-05-05 09:59:22 +0300

    lesson_7_done finish

--- CS_python_study/client_start.py
+++ CS_python_study/client_start.py
@@ -1,17 +1,19 @@
 import csv
+import random
 import subprocess
 import os
 import sys
 # from subprocess import Popen, CREATE_NEW_CONSOLE
 
-sys.path.append('../')
-PATH = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(os.getcwd(), '..'))
+# sys.path.append('../')
+# PATH = os.path.dirname(os.path.abspath(__file__))
+# sys.path.append(os.path.join(os.getcwd(), '..'))
 
 # PATH = os.path.join(PATH, 'client.log')
 p_list = []
 # args_c = ['/home/anton/PycharmProjects/Client_server/Client_server/CS_python_study','python client.py']
 # args_s = ['/home/anton/PycharmProjects/Client_server/Client_server/CS_python_study','python server.py']
+
 
 while True:
     action = input('Start 10 clients (s) / '
@@ -19,13 +21,15 @@ while True:
                  'Quit (q)')
 
     if action == 'q':
+        print('Aplication closed')
         break
     elif action == 's':
-        p_list.append(subprocess.Popen(f'gnome-terminal -- python3 server.py -n', shell=True))
+        p_list.append(subprocess.Popen(f'gnome-terminal -- python3 server.py', shell=True))
 
         for _ in range(10):
+
             p_list.append(subprocess.Popen(f'gnome-terminal -- python3 client.py -n', shell=True))
-            print('10 clients are start')
+        print('10 clients are start')
     elif action == 'x':
         for p in p_list:
             p.kill()
