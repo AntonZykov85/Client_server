7e3309b684d6253be5556432a131245b56029430
AntonZykov85 <zakupki_pomosh@mail.ru>
2022-05-05 09:36:58 +0300

    lesson_7_done finish

--- CS_python_study/client_start.py
+++ CS_python_study/client_start.py
@@ -10,8 +10,8 @@ sys.path.append(os.path.join(os.getcwd(), '..'))
 
 # PATH = os.path.join(PATH, 'client.log')
 p_list = []
-args_c = ['/home/anton/PycharmProjects/Client_server/Client_server/CS_python_study','python client.py']
-args_s = ['/home/anton/PycharmProjects/Client_server/Client_server/CS_python_study','python server.py']
+# args_c = ['/home/anton/PycharmProjects/Client_server/Client_server/CS_python_study','python client.py']
+# args_s = ['/home/anton/PycharmProjects/Client_server/Client_server/CS_python_study','python server.py']
 
 while True:
     action = input('Start 10 clients (s) / '
@@ -21,10 +21,10 @@ while True:
     if action == 'q':
         break
     elif action == 's':
-        p_list.append(subprocess.Popen((args_s), shell=True))
+        p_list.append(subprocess.Popen(f'gnome-terminal -- python3 server.py -n', shell=True))
 
         for _ in range(10):
-            p_list.append(subprocess.Popen(args_c))
+            p_list.append(subprocess.Popen(f'gnome-terminal -- python3 client.py -n', shell=True))
             print('10 clients are start')
     elif action == 'x':
         for p in p_list:
